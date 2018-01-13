6d12681fe5f7a74926502a3cc8514ab83e175e0a
suyog.godse@gmail.com <suyog.godse@gmail.com>
2018-01-13 18:42:01 -0500

    Both lines are comments

--- PRJ1/src/HelloWorld.java
+++ PRJ1/src/HelloWorld.java
@@ -2,9 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, This is line#1");
-		
-		System.out.println("Hello, This is line#2");
+	//	System.out.println("Hello, This is line#1");
+	//	System.out.println("Hello, This is line#2");
 	}
 
+	
 }
