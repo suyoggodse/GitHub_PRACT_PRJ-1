d5e18843be6de48bcb4d689f09ac25e1aaa27a87
suyog.godse@gmail.com <suyog.godse@gmail.com>
2018-01-13 18:57:04 -0500

    sdsfsdfs

--- PRJ1/src/HelloWorld.java
+++ PRJ1/src/HelloWorld.java
@@ -3,9 +3,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-	//	System.out.println("Hello, This is line#1");
-	//	System.out.println("Hello, This is line#2");
-		System.out.println("Hello, This is line#2");
 		
 
 	}
